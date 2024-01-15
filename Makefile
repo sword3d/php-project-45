5d1ac8bcb632c4b75995c927b0fef3c0e2a9ed57
Pavel B <pavel.buynovskiy@gmail.com>
2024-01-15 20:34:33 +0200

    add lint

--- Makefile
+++ Makefile
@@ -5,8 +5,16 @@ install:
 validate:
 	composer validate
 
+lint:
+	composer exec --verbose phpcs -- --standard=PSR12 src bin
+
+fix:
+	composer exec --verbose phpcbf -- --standard=PSR12 src bin
+
 brain-games:
 	./bin/brain-games
+
+
 
 # make brain-games
 # make brain-even
--- src/Cli.php
+++ src/Cli.php
@@ -5,12 +5,9 @@ namespace BrainGames\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function greeting() 
+function greeting()
 {
-	line('Welcome to the Brain Game!');
-	$name = prompt('May I have your name?');
-	line("Hello, %s!", $name);
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
 }
-
-
-
